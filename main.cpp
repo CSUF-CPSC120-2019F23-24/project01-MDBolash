1251410fd295255e793997673803d6a177a67479
MD_Bolash <MD_Bolash.csu.fullerton.edu>
2019-09-13 23:49:41 -0700

    project finished

--- main.cpp
+++ main.cpp
@@ -9,42 +9,40 @@ int main()
 {
 
 //State Variables.
-double hotel, meal, total, cost, ftotal;
+double hotel, meal, total, cost, food;
 int days;
 std::string location;
 //Intro.
-std::cout << "Welcome to the Business Trip Tracker!.";
+std::cout << "Welcome to the Business Trip Tracker! ";
 
 //Get location
-std::cout << "Where are you heading?";
+std::cout << "Where are you heading? ";
 std::cin >> location;
 //Get Var days
-std::cout << "How many days will the trip take?";
+std::cout << "How many days will the trip take? ";
 std::cin >> days;
-//Get Hotel cost
-std::cout << "How much does the hotel cost?.";
+//Get Hotel cost I'm not sure if the hotal cost was supposed to be per day but it makes sense that it is.
+std::cout << "How much does the hotel cost per day? ";
 std::cin >> hotel;
+double htotal = hotel*days ;
+// Get the food cost.
+std::cout << "how much did you spend on food a day? ";
+std::cin >> food;
+double ftotal = food*days ;
 
-  // Get the food cost.
-  std::cout << "What is the meal's 'cost' ? ";
-  std::cin >> cost;
-  double tax = cost * 0.0775;
-  double tip = cost * 0.20;
-  // Calculate the cost of the meal.
-  ftotal = cost+tax+tip;
+//calculate total for Business expenses.
+total = ftotal+htotal;
+std::cout << std::setw(17) << "location"
+      << std::setw(17) << "days"
+      << std::setw(17) << "hotel"
+      << std::setw(17) << "food"
+      << std::setw(17) << "total" << '\n' ;
+std::cout << std::setw(17) << location ;
+std::cout  << std::setw(17) << days ;
+std::cout  << std::setw(17) << htotal ;
+std::cout  << std::setw(17) << ftotal ;
+std::cout  << std::setw(17) << total ;
 
-  // Display the cost of the meal with tax and tip.
-  std::cout << "The \"ftotal\" of the bill with tax and tip included is " << total << "$.";
 
-  return 0;
-  //calculate total for Business expenses.
-  total = ftotal+hotel;
-  std::cout << std::setw(15) << "location"
-    << std::setw(17) << "days"
-    << std::setw(17) << "hotel";
-  std::cout << std::setw(15) << location;
-    <<std::setw(17) << "ftotal"
-    <<std::setw(17) << "days"
-    <<std::setw(17) << "days"
-    <<std::setw(17) << total << '\n';
+return 0;
   }
